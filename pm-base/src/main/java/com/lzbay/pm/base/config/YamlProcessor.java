8a4774d78bd1cbf0fa06776d71b2667f7cb15e8e
glintoo <glintoo@hotmail.com>
2024-11-26 23:49:03 +0800

    add pom

--- pm-base/src/main/java/com/lzbay/pm/base/config/YamlProcessor.java
+++ pm-base/src/main/java/com/lzbay/pm/base/config/YamlProcessor.java
@@ -1,2 +1,59 @@
-package com.lzbay.pm.base.config;public class YamlProcessor {
+package com.lzbay.pm.base.config;
+
+import cn.hutool.core.util.StrUtil;
+import com.lzbay.pm.base.common.constant.StringConst;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.boot.env.YamlPropertySourceLoader;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * 加载 pm-*.yml 配置文件
+ */
+@Configuration
+@Slf4j
+@Order(value = 0)
+public class YamlProcessor implements EnvironmentPostProcessor {
+
+    private final YamlPropertySourceLoader loader = new YamlPropertySourceLoader();
+
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        String filePath = environment.getProperty(StringConst.LOG_PATH);
+        if (StrUtil.isNotBlank(filePath)) {
+            System.setProperty(StringConst.LOG_PATH, filePath);
+        }
+
+        MutablePropertySources propertySources = environment.getPropertySources();
+        this.loadProperty(propertySources);
+    }
+
+    private void loadProperty(MutablePropertySources propertySources) {
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        try {
+            Resource[] resources = resolver.getResources("classpath*:pm-*.yml");
+
+            if(resources.length<1){
+                return;
+            }
+
+            for (Resource resource : resources) {
+                log.info("初始化系统配置：{}", resource.getFilename());
+                List<PropertySource<?>> load = loader.load(resource.getFilename(), resource);
+                load.forEach(propertySources::addLast);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
