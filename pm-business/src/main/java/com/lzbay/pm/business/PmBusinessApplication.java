6f84f33a5cccafc83b4cafe29024d6a31fa2610e
yiibryan <yiibryan@hotmail.com>
2024-11-28 23:36:12 +0800

    update config

---
+++ pm-business/src/main/java/com/lzbay/pm/business/PmBusinessApplication.java
@@ -0,0 +1,27 @@
+package com.lzbay.pm.business;
+
+
+import com.lzbay.pm.base.listener.LogVariableListener;
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(PmBusinessApplication.SCAN_PACKAGE)
+@MapperScan(value = PmBusinessApplication.SCAN_PACKAGE, annotationClass = Mapper.class)
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class, DataSourceAutoConfiguration.class})
+public class PmBusinessApplication {
+
+    public static final String SCAN_PACKAGE = "com.lzbay.pm";
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(PmBusinessApplication.class);
+
+        application.addListeners(new LogVariableListener());
+        application.run(args);
+    }
+
+}
