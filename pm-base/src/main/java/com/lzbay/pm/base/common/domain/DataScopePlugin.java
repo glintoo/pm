ad67c8fa107eab9f3b9e95eb23a326494e6164bc
glintoo <glintoo@hotmail.com>
2024-12-02 00:05:27 +0800

    add data source

---
+++ pm-base/src/main/java/com/lzbay/pm/base/common/domain/DataScopePlugin.java
@@ -0,0 +1,10 @@
+package com.lzbay.pm.base.common.domain;
+
+import org.apache.ibatis.plugin.Interceptor;
+
+/**
+ * 数据范围 插件
+ *
+ */
+public abstract class DataScopePlugin implements Interceptor {
+}
